f9bd5221be7b85dd56ca4a1fac407533fe722b15
kevinvaningen <kevin.van.ingen@gmail.com>
2015-11-27 20:21:51 +0100

    Added interface doc.

--- CSV-Pusher/src/main/java/nl/hr/cmi/citygis/Publishable.java
+++ CSV-Pusher/src/main/java/nl/hr/cmi/citygis/Publishable.java
@@ -8,7 +8,7 @@ public interface Publishable {
     /**
      * Method that makes a connection and publishes the message to the server topic.
      *
-     * @param topic this is the mqtt channel where messages are published
+     * @param topic   this is the mqtt channel where messages are published
      * @param message a textual String that is publisheable to MQTT
      * @return returns the status of succesfull publication. True means the message is succesfully published.
      */
@@ -20,9 +20,9 @@ public interface Publishable {
     public void disconnectFromBroker();
 
     /**
-     *
      * @return returns the connection status. True means an open socket connection.
      */
     public boolean isConnectedToServer();
+
     public void connect();
 }

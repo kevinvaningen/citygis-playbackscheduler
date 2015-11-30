d6aa33a9e3533f109f7a7891d76041b8eb534145
Youri Tjang <mail@tjang.nl>
2015-11-30 18:05:09 +0100

    changed datetime-toJSON to ISO8601

--- CSV-Pusher/src/main/java/nl/hr/cmi/citygis/models/CityGisData.java
+++ CSV-Pusher/src/main/java/nl/hr/cmi/citygis/models/CityGisData.java
@@ -1,15 +1,35 @@
 package nl.hr.cmi.citygis.models;
 
+import com.google.gson.*;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
+import java.lang.reflect.Type;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+import java.time.ZoneId;
+import java.util.Date;
 
 /**
  * Created by youritjang on 09-11-15.
  */
 public abstract class CityGisData implements iCityGisModel{
-    transient Gson gson = new GsonBuilder().create();
+//    transient Gson gson = new GsonBuilder().create();
+
+    transient GsonBuilder gsonBuilder = new GsonBuilder();
+    transient  Gson gson = gsonBuilder.create();
+
+
+    public class LocalDateTimeSerializer implements JsonSerializer<LocalDateTime> {
+        @Override
+        public JsonElement serialize(LocalDateTime localDateTime, Type type, JsonSerializationContext jsonSerializationContext) {
+            Instant instant = localDateTime.atZone(ZoneId.systemDefault()).toInstant();
+            Date date = Date.from(instant);
+            return new JsonPrimitive(date.getTime());
+        }
+    }
+
     public String toJSON(){
+        gsonBuilder.registerTypeAdapter(LocalDateTime.class, new LocalDateTimeSerializer());
         return gson.toJson(this);
     }
 
